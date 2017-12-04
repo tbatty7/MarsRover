4c8078269c48421c03a9d0d0ff57af024fbfa182
Timothy Batty <tbatty7@gmail.com>
2017-12-04 01:07:42 -0500

    Extracted for loop in Main method to moveRovers()

--- src/MarsRover.java
+++ src/MarsRover.java
@@ -3,34 +3,30 @@ import java.util.*;
 
 public class MarsRover {
     public static void main(String args[]) {
-
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner input = new Scanner(System.in);
         System.out.println("Please enter top right coordinates of Grid.");
-        String gridSize = scanner.nextLine();
+        String gridSize = input.nextLine();
         ArrayList roversArray = new ArrayList( );
         System.out.println("Please enter the rover's starting coordinates and direction.");
         Boolean haveCoordinatesBeenRequested = true;
         int numberOfRovers = 2;
+        moveRovers(input, roversArray, haveCoordinatesBeenRequested, numberOfRovers);
+        printRoverCoordinates(roversArray);
+    }
 
+    private static void moveRovers(Scanner input, ArrayList roversArray, Boolean haveCoordinatesBeenRequested, int numberOfRovers) {
         for (int i = 0; i < numberOfRovers; i++) {
             haveCoordinatesBeenRequested = checkOrRequest(haveCoordinatesBeenRequested);
-            int roverX = scanner.nextInt();
-            int roverY = scanner.nextInt();
-
-            char roverDirection = scanner.next().charAt(0);
-
+            int roverX = input.nextInt();
+            int roverY = input.nextInt();
+            char roverDirection = input.next().charAt(0);
             Rover rover = new Rover(roverX, roverY, roverDirection);
-
             System.out.println("Please enter the search pattern.");
-            String commandString = scanner.next();
+            String commandString = input.next();
             String[] commandArray = commandString.split("");
-
             executeSearchPattern(rover, commandArray);
             roversArray.add(rover.getPosition());
         }
-
-        printRoverCoordinates(roversArray);
     }
 
     private static void printRoverCoordinates(ArrayList roversArray) {
